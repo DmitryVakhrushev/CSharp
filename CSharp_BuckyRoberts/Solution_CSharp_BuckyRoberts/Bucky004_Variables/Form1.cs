fdea2fe555fc60879a083fefe063a13ff7d9a856
DmitryVakhrushev <dm.vakhrushev@gmail.com>
2019-07-12 11:17:06 -0400

    Message Box and Variables

---
+++ CSharp_BuckyRoberts/Solution_CSharp_BuckyRoberts/Bucky004_Variables/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bucky004_Variables
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string name = "Adam";
+            MessageBox.Show(name);
+
+            int number = 5;
+            MessageBox.Show(number.ToString());
+
+            bool isRedHair = false;
+            MessageBox.Show(isRedHair.ToString());
+
+            // object variable can be anythig
+            object myObj = true;
+            MessageBox.Show(myObj.ToString());
+        }
+    }
+}
