9e4a3d183619e6e14e38c7ca6e45ed2b89daa4d2
DmitryVakhrushev <dm.vakhrushev@gmail.com>
2019-07-15 16:07:23 -0400

    Changing form text and properties using code. "If" and "else if" statements.

---
+++ CSharp_BuckyRoberts/Solution_CSharp_BuckyRoberts/Bucky005_ChangingPropertiesWithCode/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bucky005_ChangingPropertiesWithCode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = "Adam";
+            button1.Height = 60;
+            button1.Enabled = false;
+
+            textBox1.Text = "Adam";
+            textBox1.MaxLength = 2;
+        }
+    }
+}
---
+++ CSharp_BuckyRoberts/Solution_CSharp_BuckyRoberts/Bucky006_IfStatements/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bucky006_IfStatements
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "Adam") // !=, 
+            {
+                MessageBox.Show("Hello");
+            }
+
+            else if(textBox1.Text == "Bob")
+            {
+                MessageBox.Show("yo");
+            }
+
+            else if (textBox1.Text == "Joe")
+            {
+                MessageBox.Show("Hi");
+            }
+            else
+            {
+                MessageBox.Show("Sorry, your name is not important");
+            }
+        }
+    }
+}
