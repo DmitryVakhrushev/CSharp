f2e67579122d0246cefa891fc024367a90e3eabc
DmitryVakhrushev <dm.vakhrushev@gmail.com>
2019-07-26 09:52:55 -0400

    If and switch statements

---
+++ CSharp_BuckyRoberts/Solution_CSharp_BuckyRoberts/Bucky009_SwitchStatements/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bucky009_SwitchStatements
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            switch(textBox1.Text)
+            {
+                case "Adam":
+                    MessageBox.Show("Hello");
+                    break;
+                case "Bob":
+                    MessageBox.Show("yo");
+                    break;
+                default:
+                    MessageBox.Show("Your name isn't important");
+                    break;
+            }
+
+            int i = 16;
+            switch(i)
+            {
+                case 4:
+                    MessageBox.Show("You are too young");
+                    break;
+                case 16:
+                    MessageBox.Show("You can drive");
+                    break;
+            }
+
+        }
+    }
+}
