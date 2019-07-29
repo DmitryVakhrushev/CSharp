4478244d1e70f4a16390eefbd0df041f8cf3189d
DmitryVakhrushev <dm.vakhrushev@gmail.com>
2019-07-29 11:42:53 -0400

    Math operators and Arrays

---
+++ CSharp_BuckyRoberts/Solution_CSharp_BuckyRoberts/Bucky010_MathematicalOperators/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bucky010_MathematicalOperators
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int a = 5;
+            int b = 3;
+
+            MessageBox.Show((a + b).ToString());
+
+            a = a + 7;
+            a += 7;
+
+            MessageBox.Show(a.ToString());
+
+            a = a + 1;
+            a++; //increment a by 1
+
+            MessageBox.Show(a.ToString());
+
+            --a; //decrement by 1 first
+        }
+
+    }
+}
---
+++ CSharp_BuckyRoberts/Solution_CSharp_BuckyRoberts/Bucky011_Arrays/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bucky011_Arrays
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            // Define a string array with hardcoded values
+            string[] Names = { "Adam", "Bob", "Joe" };
+            MessageBox.Show(Names[0]);
+
+            // Define a string array with up to 5 members
+            string[] Fruits = new string[5];
+            Fruits[0] = "Banana";
+            MessageBox.Show(Fruits[0]);
+
+            // Define an integer array with hardcoded values
+            int[] Numbers = { 123, 456, 789 };
+            MessageBox.Show(Numbers[1].ToString());
+        }
+    }
+}
