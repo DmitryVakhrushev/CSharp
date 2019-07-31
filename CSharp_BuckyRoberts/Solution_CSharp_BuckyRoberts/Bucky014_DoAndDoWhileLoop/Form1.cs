90937447cb040cfc752576a69a4cd4530536d010
DmitryVakhrushev <dm.vakhrushev@gmail.com>
2019-07-31 09:44:35 -0400

    Do and DoWhile loops. Try-Catch-Finally.

---
+++ CSharp_BuckyRoberts/Solution_CSharp_BuckyRoberts/Bucky014_DoAndDoWhileLoop/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bucky014_DoAndDoWhileLoop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            while (i < 10) // while (true) creates an infinite loop
+            {
+                textBox1.Text += i.ToString();
+                i++;
+            }
+
+            //DO-WHILE executes the code at least once
+            do
+            {
+                textBox1.Text += i.ToString();
+                i++;
+            }
+            while (i < 10);
+        }
+    }
+}
---
+++ CSharp_BuckyRoberts/Solution_CSharp_BuckyRoberts/Bucky015_TryCatchFinally/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bucky015_TryCatchFinally
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try //checks if error occurs
+            {
+                string[] names = new string[2]; //This array has only two elements with indexes 0 and 1.
+                string s = names[2]; //We try to access the third element that's not exist
+            }
+
+            catch (Exception ex) //executes on error only
+            {
+                MessageBox.Show("There was an error: " + ex.Message);
+            }
+
+            finally //executes regardless of the error
+            {
+                MessageBox.Show("Your code is done");
+            }
+        }
+    }
+}
