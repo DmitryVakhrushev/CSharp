cf8d35dad1762c824f9b1568848513fdbad4078d
DmitryVakhrushev <dm.vakhrushev@gmail.com>
2019-07-31 08:51:21 -0400

    Lists. For and ForEach loops.

---
+++ CSharp_BuckyRoberts/Solution_CSharp_BuckyRoberts/Bucky012_Lists/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bucky012_Lists
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //ARRAY
+            string[] myArray = new string[4]; //array requires the 
+
+            //LIST
+            //List of strings
+            List<string> Names = new List<string>(); //list
+            Names.Add("Adam");
+            Names.Add("Joe");
+            MessageBox.Show(Names[0]); //access the first element of the list
+
+            //List of integers
+            List<int> numbers = new List<int>();
+            numbers.Add(5);
+            numbers.Add(598);
+            MessageBox.Show(numbers[1].ToString());
+        }
+    }
+}
---
+++ CSharp_BuckyRoberts/Solution_CSharp_BuckyRoberts/Bucky013_ForAndForeachLoop/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bucky013_ForAndForeachLoop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //"For" Loop
+            for (int i=0; i < 5; i++)
+            {
+                MessageBox.Show("Hello" + i.ToString());
+            }
+
+            //"ForEach" Loop allows you to loop through every element inside of an array or list
+
+            //Loop through the array
+            string[] names = { "Adam", "Bob", "Joe" };
+            foreach (string s in names)
+            {
+                MessageBox.Show("Hello " + s);
+            }
+
+            //Loop through the list
+            List<int> numbers = new List<int>();
+            numbers.Add(5);
+            numbers.Add(10);
+            numbers.Add(15);
+
+            foreach (int i in numbers)
+            {
+                MessageBox.Show("The list number = " + i.ToString());
+            }
+
+
+        }
+    }
+}
