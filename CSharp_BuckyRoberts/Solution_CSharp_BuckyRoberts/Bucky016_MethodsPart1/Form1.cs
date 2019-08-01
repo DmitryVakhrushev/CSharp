d8dacb80b7a0b197eab15745d44e4efd5cc31c16
DmitryVakhrushev <dm.vakhrushev@gmail.com>
2019-08-01 10:01:55 -0400

    Methods

---
+++ CSharp_BuckyRoberts/Solution_CSharp_BuckyRoberts/Bucky016_MethodsPart1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bucky016_MethodsPart1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Message(); //Call the method "Message" from the Button1
+            MessageWithString("Adam");
+        }
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            MessageMultiParam("Hi Bob!","Grettings");
+        }
+
+        //----------------- My Methods -------------------
+        //Method #1
+        void Message()
+        {
+            MessageBox.Show("Hello");
+        }
+
+        //Method #2
+        void MessageWithString(string message)
+        {
+            MessageBox.Show(message);
+        }
+
+        //Method #3
+        void MessageMultiParam(string message, string myTitle)
+        {
+            MessageBox.Show(message, myTitle);
+        }
+
+    }
+}
---
+++ CSharp_BuckyRoberts/Solution_CSharp_BuckyRoberts/Bucky017_MethodsPart2/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bucky017_MethodsPart2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(MyMethod2() + " " + MyMethod("Adam") + " " + MyMethod3().ToString());
+        }
+        
+        //==================== My Methods ====================
+        /* Return type
+         * void - returns nothing
+         * string - returns a specified string
+        */
+        string MyMethod(string name)
+        {
+            return name;
+        }
+
+        //MyMethod2 returns a static STRING value
+        string MyMethod2()
+        {
+            return "Hello";
+        }
+
+        //MyMethod3 returns a static INT value
+        int MyMethod3()
+        {
+            return 27;
+        }
+
+    }
+}
