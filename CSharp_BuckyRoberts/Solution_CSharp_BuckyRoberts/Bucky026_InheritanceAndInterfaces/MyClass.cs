4b0529c6de36af2c16fc7b6c9763adc3ce71b907
DmitryVakhrushev <dm.vakhrushev@gmail.com>
2019-08-12 10:00:48 -0400

    Iheritance andInterfaces

---
+++ CSharp_BuckyRoberts/Solution_CSharp_BuckyRoberts/Bucky026_InheritanceAndInterfaces/MyClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+
+namespace MyNamespace
+{
+    class MyClass
+    {
+        public string name = "Bob";
+        public int age = 10;
+        public virtual void ShowMessage(string Message)
+        {
+            System.Windows.Forms.MessageBox.Show(Message);
+        }
+    }
+
+
+    //inside of this interface you can create names for methods, properties, indexers, and events
+    interface IMyInterface
+    {
+        void MyVoid();
+
+    }
+
+    //Inheritance takes all non-private members of the original class to use in another class
+    class MySecondClass : MyClass, IMyInterface // MyClass is the "base" class
+    {
+        public string hairColor = "Brown";
+        public override void ShowMessage(string message)
+        {
+            System.Windows.Forms.MessageBox.Show(message, "My Title");
+        }
+
+        public void MyVoid()
+        {
+            base.ShowMessage("My void");
+        }
+    }
+}
