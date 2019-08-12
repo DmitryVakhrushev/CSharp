5c536e99f2bfa731f60d10f2cdfef87c649bfaf7
DmitryVakhrushev <dm.vakhrushev@gmail.com>
2019-08-12 10:01:05 -0400

    Indexers

---
+++ CSharp_BuckyRoberts/Solution_CSharp_BuckyRoberts/Bucky027_Indexers/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MyNamespace;
+
+namespace Bucky027_Indexers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            MyClass mc = new MyClass();
+            MessageBox.Show(mc[0]);
+
+            mc[1] = "Bryan"; //set a new value for the second array element
+            MessageBox.Show(mc[1]);
+        }
+    }
+}
---
+++ CSharp_BuckyRoberts/Solution_CSharp_BuckyRoberts/Bucky027_Indexers/MyClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNamespace
+{
+    class MyClass
+    {
+
+        string[] myArray = { "Adam", "Bob", "Joe" };
+        
+        //Indexer
+        public string this[int index]
+        {
+            get { return myArray[index]; }
+            set { myArray[index] = value; }
+        }
+
+    }
+}
