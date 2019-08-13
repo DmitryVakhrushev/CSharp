932645198041dd985367b0be81829c11cdaf055e
DmitryVakhrushev <dm.vakhrushev@gmail.com>
2019-08-13 09:58:40 -0400

    Structs and Partial.

---
+++ CSharp_BuckyRoberts/Solution_CSharp_BuckyRoberts/Bucky028_Structures/MyClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNamespace
+{
+        struct Client
+        {
+            //Constructor
+            public Client(string name)
+            {
+            Name = "";
+            Age = 0;
+            }   
+            public string Name;
+            public int Age;
+
+        //We can have methods inside of Structures
+        public void ClearClientInfo()
+        {
+            Name = "";
+            Age = 0;
+        }
+        }
+}
---
+++ CSharp_BuckyRoberts/Solution_CSharp_BuckyRoberts/Bucky029_Partial/PartialMyClass2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNamespace
+{
+        partial class MyClass
+        {
+            public string HairColor = "Brown";
+            public bool Glasses = false;
+
+        partial void Message(string message)
+        {
+            System.Windows.Forms.MessageBox.Show(message);
+        }
+
+        public void ShowMessage(string message)
+        {
+            Message(message);
+        }
+
+    }
+}
