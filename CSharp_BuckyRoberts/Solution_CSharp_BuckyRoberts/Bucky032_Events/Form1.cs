1441b8298f20c0fe7dbdf32a0fdffab2cd4f46bb
DmitryVakhrushev <dm.vakhrushev@gmail.com>
2019-08-19 10:00:52 -0400

    Events and Ternary Operator.

---
+++ CSharp_BuckyRoberts/Solution_CSharp_BuckyRoberts/Bucky032_Events/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MyNamespace;
+
+namespace Bucky032_Events
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            MyClass mc = new MyClass();
+            mc.OnPropertyChanged += Mc_OnPropertyChanged;
+            mc.Name = "Adam"; //changing the property
+        }
+
+        private void Mc_OnPropertyChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("The property has changed");
+        }
+    }
+}
---
+++ CSharp_BuckyRoberts/Solution_CSharp_BuckyRoberts/Bucky032_Events/MyClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNamespace
+{
+    class MyClass
+    {
+
+        public event EventHandler OnPropertyChanged;
+
+        string name = "";
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                //when a user changes a property value we raise the event
+                name = value;
+                OnPropertyChanged(this, new EventArgs());
+            }
+        }
+    }
+}
